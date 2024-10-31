3b7c29b21a6e84be25ea9a1d21b5c1371da67737
joshfamd <166756874+joshfamd@users.noreply.github.com>
2024-10-31 11:49:57 -0700

    Update smci_mi300x_platform_amdgpu_alllogs_collection.sh

--- smci_mi300x_platform_amdgpu_alllogs_collection.sh
+++ smci_mi300x_platform_amdgpu_alllogs_collection.sh
@@ -1,6 +1,5 @@
-#Copyright(C) 2024 Advanced Micro Devices, Inc. All rights reserved.
-
 #!/bin/bash
+#Copyright(C) 2024 Advanced Micro Devices, Inc. All rights reserved.
  
 # Function for logging with timestamps
 log() {
