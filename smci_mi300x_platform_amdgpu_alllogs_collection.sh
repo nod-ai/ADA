9a8147817dbf50f35e4bce9e35c5a6be5f3b9825
jatreon <justin.treon@amd.com>
2025-01-16 14:10:32 -0800

    Adding pulling the CPERs and the events logs to the smci_mi300x_platform_amdgpu_alllogs_collection.sh script.

--- smci_mi300x_platform_amdgpu_alllogs_collection.sh
+++ smci_mi300x_platform_amdgpu_alllogs_collection.sh
@@ -109,7 +109,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Step 1: Collect Diagnostic Data
-log "Collecting diagnostic data..."
+log "Collecting AllLogs file..."
 taskResponse=$(curl -s -k -u "${BMC_USERNAME}:${BMC_PASSWORD}" \
     "https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/DiagLogs/Actions/LogService.CollectDiagnosticData" \
     -X POST -d '{"DiagnosticDataType":"OEM", "OEMDiagnosticDataType" : "AllLogs"}')
@@ -169,14 +169,120 @@ fi
 # Step 5: Download All Logs to logs directory
 OUTPUT_DIR="logs"
 mkdir -p "${OUTPUT_DIR}"
-log "Downloading all logs..."
+log "Downloading AllLogs..."
 
 attachment=$(curl -k -s -u "${BMC_USERNAME}:${BMC_PASSWORD}" -X GET \
     -H 'Content-Type: application/json' -H 'Accept: application/json' \
     https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/DiagLogs/Entries |
     python3 -c "import sys, json; print(json.load(sys.stdin)['Members'][${entryId}]['AdditionalDataURI'])")
 
+filename="${OUTPUT_DIR}/${BMC_IP}_$(date +"%Y-%m-%dT%H-%M-%S")_all_logs.tar.xz"
 curl -k -s -u "${BMC_USERNAME}:${BMC_PASSWORD}" -X GET \
-    https://${BMC_IP}${attachment} >"${OUTPUT_DIR}/all_logs.tar.xz"
+    https://${BMC_IP}${attachment} >${filename}
 check_curl_error "Failed to download logs."
-log "All logs downloaded as ${OUTPUT_DIR}/all_logs.tar.xz"
+log "All logs downloaded as ${filename}"
+
+# Step 6: Collecting CPERs
+log "Collecting diagnostic data..."
+taskResponse=$(curl -s -k -u "${BMC_USERNAME}:${BMC_PASSWORD}" \
+    "https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/DiagLogs/Actions/LogService.CollectDiagnosticData" \
+    -X POST -d '{"DiagnosticDataType":"OEM", "OEMDiagnosticDataType" : "AllCPERs"}')
+check_curl_error "Failed to collect CPER data."
+
+# Extract Task ID
+TASKS=$(echo "${taskResponse}" | grep -oP '(?<=Tasks/)[^"]*')
+
+if [[ ${TASKS} == "" ]]; then
+    log "Script failed, task ID has no value"
+    exit 1
+fi
+
+# Step 7: Poll for all tasks to complete
+tasksWait
+
+entries=$(curl -k -s -u "${BMC_USERNAME}:${BMC_PASSWORD}" -X GET \
+    -H 'Content-Type: application/json' -H 'Accept: application/json' \
+    https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/DiagLogs/Entries |
+    python3 -c "import sys, json; print(json.load(sys.stdin)['Members@odata.count'])")
+
+if [[ ${entries} == "0" ]]; then
+    log "Script failed, logs not found"
+    exit 1
+fi
+
+idGreatest=0
+entryGreatest=0
+
+# Step 8: Go through the entries and find the one with the greatest ID
+for ((entry = $((entries - 1)); entry >= 0; entry--)); do
+    entryId=$(curl -k -s -u "${BMC_USERNAME}:${BMC_PASSWORD}" -X GET \
+        -H 'Content-Type: application/json' -H 'Accept: application/json' \
+        https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/DiagLogs/Entries |
+        python3 -c "import sys, json; print(json.load(sys.stdin)['Members'][${entry}]['Id'])")
+
+    if [ ${idGreatest} -le ${entryId} ]; then
+        idGreatest=${entryId}
+        entryGreatest=${entry}
+    fi
+done
+
+entryId=${entryGreatest}
+log "Entry ID: ${entryId}"
+
+# Step 9: Check to make sure the entry is an AllCPERs
+entryType=$(curl -k -s -u "${BMC_USERNAME}:${BMC_PASSWORD}" -X GET \
+    -H 'Content-Type: application/json' -H 'Accept: application/json' \
+    https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/DiagLogs/Entries |
+    python3 -c "import sys, json; print(json.load(sys.stdin)['Members'][${entryGreatest}]['OEMDiagnosticDataType'])")
+
+if [[ ${entryType} != "AllCPERs" ]]; then
+    log "Entry ID ${entryId} is of type ${entryType}, rather than AllCPERs"
+    exit 1
+fi
+
+# Step 10: Download All Logs to logs directory
+OUTPUT_DIR="logs"
+mkdir -p "${OUTPUT_DIR}"
+log "Downloading CPERs logs..."
+
+attachment=$(curl -k -s -u "${BMC_USERNAME}:${BMC_PASSWORD}" -X GET \
+    -H 'Content-Type: application/json' -H 'Accept: application/json' \
+    https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/DiagLogs/Entries |
+    python3 -c "import sys, json; print(json.load(sys.stdin)['Members'][${entryId}]['AdditionalDataURI'])")
+
+filename="${OUTPUT_DIR}/${BMC_IP}_$(date +"%Y-%m-%dT%H-%M-%S")_CPERs.tar.xz"
+curl -k -s -u "${BMC_USERNAME}:${BMC_PASSWORD}" -X GET \
+    https://${BMC_IP}${attachment} >${filename}
+check_curl_error "Failed to download CPERs logs."
+log "All CPERs downloaded as ${filename}"
+
+# Step 11: Get number of events
+log "Collecting events..."
+events=$(curl -m 20 -s -k -u "${BMC_USERNAME}:${BMC_PASSWORD}" \
+    -H 'Content-Type: application/json' -H 'Accept: application/json' \
+    "https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/EventLog/Entries" |
+    python3 -c "import sys, json; print(json.load(sys.stdin)['Members@odata.count'])")
+
+if [[ ${events} == "" ]]; then
+    log "Unable to read number of events, read events=${events}"
+    exit 1
+fi
+
+log "${events} events in logs"
+
+# If number of events more than 1000, needing to change to next page for event log
+additionalEventLogs=$(((events - 1) / 1000))
+# printf "%s\n" "${additionalEventLogs} additional gets
+
+# Step 12: Loop for page of event log
+for ((i = 0; i <= additionalEventLogs; i++)); do
+    filename="${OUTPUT_DIR}/${BMC_IP}_$(date +"%Y-%m-%dT%H-%M-%S")_event_log_${i}.json"
+    printf "%s\n" "Reading https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/EventLog/Entries?\$skip=$((1000 * i))"
+    curl -m 20 -s -k -u "${BMC_USERNAME}:${BMC_PASSWORD}" \
+        -H 'Content-Type: application/json' -H 'Accept: application/json' \
+        "https://${BMC_IP}/redfish/v1/Oem/Supermicro/MI300X/Systems/UBB/LogServices/EventLog/Entries?\$skip=$((1000 * i))" |
+        python3 -c "import sys, json; print(json.dumps(json.load(sys.stdin)['Members'], indent=4))" >${filename}
+    check_curl_error "Failed to download events logs."
+    log "Events log ${i} downloaded as ${filename}"
+done
+
